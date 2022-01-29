7e850dbcd3043fddd9367da715c5bd15a920b437
bgtdinh <bgtdinh@gmail.com>
2022-01-28 17:15:45 -0800

    Added raid selection

--- pages/index.js
+++ pages/index.js
@@ -3,25 +3,43 @@ import socketIoClient from 'socket.io-client';
 import React from 'react';
 import Tweet from '../components/Tweet';
 import Navbar from '../components/Navbar';
+import RaidSelection from '../components/RaidSelection';
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       tweets: [],
-      columnOne: false,
-      columnTwo: false,
-      columnThree: false,
-      columnOneFilter: 'Proto Bahamut',
-      columnTwoFilter: 'Grand Order',
-      columnThreeFilter:'Akasha',
+      currentColumnClicked: 0,
+      columnOneFilter: '',
+      columnTwoFilter: '',
+      columnThreeFilter:'',
+      showModal: false,
     }
     this.handleAddColumnClick = this.handleAddColumnClick.bind(this);
     this.filterTweets = this.filterTweets.bind(this);
+    this.showModal = this.showModal.bind(this);
+    this.handleChangeColumnFilter = this.handleChangeColumnFilter.bind(this);
+  }
+
+  showModal() {
+    this.setState({showModal: !this.state.showModal});
+  }
+
+  handleChangeColumnFilter (filterValue) {
+    const map = {
+      1:'columnOneFilter',
+      2:'columnTwoFilter',
+      3: 'columnThreeFilter',
+    }
+    const columnName = map[this.state.currentColumnClicked];
+
+    this.setState({[columnName]: filterValue});
   }
 
   handleAddColumnClick(event) {
-    this.setState({[event.target.id]: !this.state[event.target.id]})
+    this.setState({currentColumnClicked: parseInt(event.target.id)});
+    this.showModal();
   }
 
   filterTweets(filter, columnTweets) {
@@ -48,7 +66,6 @@ class Home extends React.Component {
   }
 
 
-  //this.state.tweets.map( (tweet) => <Tweet key={tweet.data.id} tweet={tweet.data}/>).reverse()
 render() {
   return (
     <div className="h-screen">
@@ -59,11 +76,11 @@ render() {
               <div className="m-1 px-1 py-5 rounded bg-slate-800 shadow-xl flex justify-center">
                 <span className="text-white font-bold text-2xl">{this.state.columnOneFilter}</span>
               </div>
-              {this.state.tweets.filter( (tweet) => tweet.data.text.includes(this.state.columnOneFilter))
-              .map( (tweet) => <Tweet key={tweet.data.id} tweet={tweet.data} />).reverse() }
+              {this.state.columnOneFilter !== '' ? this.state.tweets.filter( (tweet) => tweet.data.text.includes(this.state.columnOneFilter))
+              .map( (tweet) => <Tweet key={tweet.data.id} tweet={tweet.data} />).reverse() : <></>}
             </div>
-            <div id="columnOne" className="m-4 p-5 bg-slate-600 flex justify-center rounded-xl hover:bg-slate-400" onClick={this.handleAddColumnClick}>
-              <span id="columnOne" className="text-white text-3xl" onClick={this.handleAddColumnClick}>+</span>
+            <div id="1" className="m-4 p-5 bg-slate-600 flex justify-center rounded-xl hover:bg-slate-400" onClick={this.handleAddColumnClick}>
+              <span id="1" className="text-white text-3xl" onClick={this.handleAddColumnClick}>+</span>
             </div>
           </div>
           <div className="flex-1 flex flex-col bg-slate-500 justify-between">
@@ -71,11 +88,11 @@ render() {
             <div className="m-1 px-1 py-5 rounded bg-slate-800 shadow-xl flex justify-center">
                 <span className="text-white font-bold text-2xl">{this.state.columnTwoFilter}</span>
               </div>
-            {this.state.tweets.filter( (tweet) => tweet.data.text.includes(this.state.columnTwoFilter))
-              .map( (tweet) => <Tweet key={tweet.data.id} tweet={tweet.data} />).reverse() }
+            {this.state.columnTwoFilter !== '' ? this.state.tweets.filter( (tweet) => tweet.data.text.includes(this.state.columnTwoFilter))
+              .map( (tweet) => <Tweet key={tweet.data.id} tweet={tweet.data} />).reverse() : <></>}
             </div>
-            <div id="columnTwo" className="m-4 p-5 bg-slate-600 flex justify-center rounded-xl hover:bg-slate-400" onClick={this.handleAddColumnClick}>
-              <span id="columnTwo" onClick={this.handleAddColumnClick} className="text-white text-3xl">+</span>
+            <div id="2" className="m-4 p-5 bg-slate-600 flex justify-center rounded-xl hover:bg-slate-400" onClick={this.handleAddColumnClick}>
+              <span id="2" onClick={this.handleAddColumnClick} className="text-white text-3xl">+</span>
             </div>
           </div>
           <div className="flex-1 flex flex-col bg-slate-500 justify-between ">
@@ -83,14 +100,24 @@ render() {
             <div className="m-1 px-1 py-5 rounded bg-slate-800 shadow-xl flex justify-center">
                 <span className="text-white font-bold text-2xl">{this.state.columnThreeFilter}</span>
               </div>
-            {this.state.tweets.filter( (tweet) => tweet.data.text.includes(this.state.columnThreeFilter))
-              .map( (tweet) => <Tweet key={tweet.data.id} tweet={tweet.data} />).reverse() }
+            {this.state.columnThreeFilter !== '' ? this.state.tweets.filter( (tweet) => tweet.data.text.includes(this.state.columnThreeFilter))
+              .map( (tweet) => <Tweet key={tweet.data.id} tweet={tweet.data} />).reverse() : <></>}
             </div>
-            <div id="columnThree" onClick={this.handleAddColumnClick} className="m-4 p-5 bg-slate-600 flex justify-center rounded-xl hover:bg-slate-400">
-              <span id="columnThree" onClick={this.handleAddColumnClick} className="text-white text-3xl">+</span>
+            <div id="3" onClick={this.handleAddColumnClick} className="m-4 p-5 bg-slate-600 flex justify-center rounded-xl hover:bg-slate-400">
+              <span id="3" onClick={this.handleAddColumnClick} className="text-white text-3xl">+</span>
             </div>
           </div>
         </div>
+        {this.state.showModal ?
+          <div name="modal background" className="bg-black bg-opacity-50 absolute inset-0 flex justify-center items-center">
+            <div name="modal content" className="m-5 p-5 bg-slate-500 rounded-xl">
+              <RaidSelection handleChangeColumnFilter={this.handleChangeColumnFilter} showModal={this.showModal}/>
+            </div>
+          </div>
+          :
+          <>
+          </>
+        }
     </div>
   );
 
